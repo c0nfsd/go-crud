176a019bb0ab1cb6b50ab3dcdf696e9bb8978931
c0nfsd <pathakmilindd@gmail.com>
2022-08-09 20:16:40 +0530

    initializers

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"log"
+	"go-crud/initializers"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	initializers.LoadEnvVariables()
 }
 
 func main() {
